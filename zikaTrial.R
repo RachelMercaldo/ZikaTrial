0727519b6a6c0a7fb3cb55c7f9a4b3c56bcd5b67
Rachel Mercaldo <rmpb13@uga.edu>
2017-08-24 05:58:11 -0400

    corrected survival time

--- zikaTrial.R
+++ zikaTrial.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(magrittr)
-library(GGally)
-library(purrr)
 library(data.table)
 library(mefa)
 library(gmodels)
+library(survival)
+
+set.seed(628496)
 
 PAHOdata<-read.csv("PAHOdata.csv") #Andersen lab data from 1/3/2016 thru 5/21/2017, 73 wks, https://github.com/andersen-lab/Zika-cases-PAHO
 PAHOdata$date<-as.Date(PAHOdata$date,"%m/%d/%Y")
@@ -14,18 +15,20 @@ PAHOdata[,2:11]<-(PAHOdata[,2:11]*4.5)/popSize
 region <- colnames(PAHOdata[,-1])
 
 
-
 makePop <- function(trial,regSize){
+  trial %<>% group_by(region) %<>% nest(.key = 'pop')
   pop<-list()
   for(i in trial$region){
     ID = paste(substr(i, 1, 2),1:regSize,sep='')
     pop[i]<-data.frame(ID)
   }
   names(pop)<-paste(trial$region)
-  pop
+  trial$pop<-pop
+  trial
 }
 
-
+#trial <- data.frame(region)
+#trial<-makePop(trial,300)
 
 getIndRR <- function(trial,regSize){
   for(reg in 1:length(trial$pop)){
@@ -35,47 +38,88 @@ getIndRR <- function(trial,regSize){
   trial
 }
 
+#trial<-getIndRR(trial,300)
 
-getImmunity <- function(trial, regSize){
+randomize <- function(trial, regSize){
   for(reg in 1:length(trial$pop)){
     trial$pop[[reg]]$immuneStatus <- factor(rbinom(regSize, 1, prob = 0.500), labels = c('vaccine','control'))
     colnames(trial$pop[[reg]])<-c("ID","indRR","studyArm")
   }
+  trial <- suppressWarnings(as.data.frame(unnest(trial))) #suppress bind_rows warning (coerce factor to character)
   trial
 }
 
+#trial<-randomize(trial,300)
 
-mergeData<-function(paho, trial,regSize){
+mergeData<-function(paho, trial,regSize){   #merge PAHO rates and trial df
   paho<-rep(PAHOdata,regSize)
-  paho<-gather(paho,'region','rate',2:11)
+  paho<-gather(paho,'region','regRate',2:11)
   paho$region<-as.factor(paho$region)
   trial<-as.data.frame(rep(trial,each=nrow(PAHOdata)))
   trial<-cbind(trial,paho)
 }
 
+#trial<-mergeData(paho,trial,300)
 
-rexpRate<-function(trial,vaccEff){
-  trial$rexpRates<-ifelse(trial$date<'2016-01-31',trial$indRR*trial$rate,
-                          trial$indRR*trial$rate*ifelse(trial$studyArm=='vaccine',1-vaccEff,1))
+totalRate<-function(trial,immuneDate = '2016-02-28', vaccEff){
+  trial$totalRate<-ifelse(trial$date < as.Date(immuneDate),trial$indRR*trial$regRate,
+                          trial$indRR*trial$regRate*ifelse(trial$studyArm=='vaccine',1-vaccEff,1))
   trial
 }
 
-getInfTimes<-function(trial){
-  trial$infTime<-rexp(nrow(trial),rate=trial$rexpRates)
+simInf<-function(trial){
+  trial<-trial[trial$totalRate != 0,]
+  trial$infectTime<-rexp(nrow(trial),rate=trial$totalRate)
+  trial
+}
+
+
+getSurvTime<-function(trial,immuneDate = '2016-02-28',endDate=max(trial$date)){
+  trial<-trial[,c(5,1,2,3,4,7,8,9)] #reordering columns for pretty-ness and removing duplicate 'region'
+   #remove rows with totalRate = 0
+  
+  preImmune<-trial[trial$date < as.Date(immuneDate),]
+  preImmune<-preImmune[preImmune$infectTime<=1,]
+  preImmune<-preImmune[!duplicated(preImmune$ID),]
+  preImmune$survt<-(preImmune$date - as.Date(immuneDate)) + 7*preImmune$infectTime
+  
+  trial<-trial[!(trial$date < as.Date(immuneDate)) & !(trial$ID %in% preImmune$ID),] 
+  
+  infected<-trial[trial$infectTime<=1,]
+  infected<-infected[!duplicated(infected$ID),] 
+  infected$survt<-(infected$date - as.Date(immuneDate)) + 7*infected$infectTime
+  
+  notInfected<-trial[trial$infectTime>1 & !(trial$ID %in% infected$ID),]
+  notInfected<-notInfected[!duplicated(notInfected$ID),] 
+  notInfected$survt<-(as.Date(endDate)-as.Date(immuneDate))
+ 
+  trial<-rbind(infected,notInfected)
+  trial<-trial[order(trial$date,trial$region),]
+  trial$survt<-as.numeric(trial$survt)
+  
+  preAndTrial<-list(pre=preImmune,trial=trial)
+  preAndTrial
+}
+
+
+getSurvStatus<-function(trial){
+  trial$status <- ifelse(trial$infectTime <= 1, 1, 0)
   trial
 }
 
 
 trial <- data.frame(region)
-trial %<>% group_by(region) %<>% nest(.key = 'pop')
-trial$pop<-makePop(trial,300)
+trial<-makePop(trial,300)
 trial <- getIndRR(trial,300)
-trial<-getImmunity(trial,300)
-trial <- as.data.frame(unnest(trial))
+trial<-randomize(trial,300)
 trial<-mergeData(paho,trial,300)
-trial<-rexpRate(trial,0.8)
-trial<-getInfTimes(trial)
+trial<-totalRate(trial,vaccEff=0.8)
+trial<-simInf(trial)  
 
-infected<-na.omit(trial[trial$infTime<=1,])
-infected<-infected[!duplicated(infected[,2]),]
-CrossTable(infected$studyArm)
+both<-getSurvTime(trial)
+preImmune<-both$pre
+trial<-both$trial
+trial<-getSurvStatus(trial)
+
+survdat<-trial[,c(3,5,9,10)]
+
